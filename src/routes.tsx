7f3ed59ce762bc7566118bd71476537109baae79
visko93 <vskonicezny@gmail.com>
2020-12-29 18:06:33 -0300

    Fet|001- First Commit

---
+++ src/routes.tsx
@@ -0,0 +1,12 @@
+//import * as React from 'react';
+import { Route, Switch } from 'react-router-dom'
+
+function Routes() {
+  return (
+    <Switch>
+      <Route></Route>
+    </Switch>
+  )
+}
+
+export default Routes
